77dd1df3b9a82293e424b444c6ecc2ab8773669e
UglyOne <tomasz@gnatonski.com>
2020-06-07 19:12:20 +0200

    making a console with a web-server in f# (the same server as written in c#)

---
+++ serverFsharp/Program.fs
@@ -0,0 +1,34 @@
+﻿open System
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.AspNetCore.SignalR
+
+type SomeHub() = 
+    inherit Hub() // inherit from Hub<a'> I think I will get type safety - https://www.codesuji.com/2019/02/19/Building-Game-with-SignalR-and-F/
+
+    member x.SendMessage arg1 arg2 = 
+        printfn "%s %s" arg1 arg2
+        x.Clients.All.SendAsync("ReceiveMessage", "from server", arg2) |> ignore
+
+type Startup() =
+    member __.ConfigureServices(services: IServiceCollection) =
+        services.AddSignalR() |> ignore
+
+    member __.Configure (app : IApplicationBuilder) =
+        app.UseRouting() |> ignore
+        app.UseEndpoints( fun builder -> builder.MapHub<SomeHub>("SomeHub") |> ignore ) |> ignore
+        ()
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    let host =
+        WebHostBuilder()
+          .UseKestrel()
+          .UseStartup<Startup>()
+          .Build()
+
+    host.Run()
+  
+    0 // return an integer exit code
